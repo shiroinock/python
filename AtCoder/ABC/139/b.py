5e750fa7890aaaf64419c2c65f164a68d0f0c40f
shirousagi <shirousagi@shirousagi-no-MacBook-Pro.local>
2019-09-03 00:22:05 +0900

    update

--- AtCoder/ABC/139/b.py
+++ AtCoder/ABC/139/b.py
@@ -1,14 +1,17 @@
 a, b = map(int, input().split())
 
-count = 1 
-
-kuchi = a
-
-while kuchi < b :
-    count+=1
-    kuchi = (a - 1) * count + 1
+if b == 1 :
+    print(0)
 else :
-    print(count)
+    count = 1
 
+    kuchi = a
+
+    while kuchi < b :
+        count+=1
+
+        kuchi = (a - 1) * count + 1
+    else :
+        print(count)
 
 """要求数１の時は例外処理だよね"""
