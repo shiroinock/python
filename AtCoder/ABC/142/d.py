ec5af86d7a170c1942def2acc3cfea7cd3d167ae
shiroinock# <k.i.wrd.setuna.g@gmail.com>
2019-09-29 02:08:27 +0900

    update

--- AtCoder/ABC/142/d.py
+++ AtCoder/ABC/142/d.py
@@ -6,28 +6,26 @@ while b:
 
 # aが最大公約数になっている
 
-def factorization(a):
-    arr = []
-    temp = a
-    for i in range(2, int(-(-a**0.5//1))+1):
-        if temp%i==0:
-            cnt=0
-            while temp%i==0:
-                cnt+=1
-                temp //= i
-            arr.append([i, cnt])
-
-    if temp!=1:
-        arr.append([temp, 1])
-
-    if arr==[]:
-        arr.append([a, 1])
-
-    return arr
-
-soinsuu = len(factorization(a))
-
-if soinsuu == 1 :
+if a == 1 :
     print(1)
 else :
-    print(soinsuu + 1)
+    def factorization(a):
+        arr = []
+        temp = a
+        for i in range(2, int(-(-a**0.5//1))+1):
+            if temp%i==0:
+                cnt=0
+                while temp%i==0:
+                    cnt+=1
+                    temp //= i
+                arr.append([i, cnt])
+
+        if temp!=1:
+            arr.append([temp, 1])
+
+        if arr==[]:
+            arr.append([a, 1])
+
+        return arr
+
+    print(int(len(factorization(a)) + 1))
