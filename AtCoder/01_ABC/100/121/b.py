0cc6745c37e248d41566af719eb8ef822987d832
shiroino <k.i.wrd.setuna.g@gmail.com>
2019-11-09 21:01:26 +0900

    update

---
+++ AtCoder/01_ABC/100/121/b.py
@@ -0,0 +1,15 @@
+n, m, c = map(int, input().split())
+
+b = list(map(int, input().split()))
+
+count = 0
+
+for i in range(n) :
+    sum = c
+    a = list(map(int, input().split()))
+    for j in range(m) :
+        sum += a[j] * b[j]
+    if sum > 0 :
+        count += 1
+
+print(count)
