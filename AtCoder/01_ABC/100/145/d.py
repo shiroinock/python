9f4542bb176a29e9a3922e30e0d3b6f395b2a481
shiroino <k.i.wrd.setuna.g@gmail.com>
2019-11-16 21:45:14 +0900

    update

---
+++ AtCoder/01_ABC/100/145/d.py
@@ -0,0 +1,26 @@
+x, y = map(int, input().split())
+
+def cmb(n, r, mod):
+    if ( r<0 or r>n ):
+        return 0
+    r = min(r, n-r)
+    return g1[n] * g2[r] * g2[n-r] % mod
+
+mod = 10**9+7 #出力の制限
+N = 10**6
+g1 = [1, 1] # 元テーブル
+g2 = [1, 1] #逆元テーブル
+inverse = [0, 1] #逆元テーブル計算用テーブル
+
+for i in range( 2, N + 1 ):
+    g1.append( ( g1[-1] * i ) % mod )
+    inverse.append( ( -inverse[mod % i] * (mod//i) ) % mod )
+    g2.append( (g2[-1] * inverse[-1]) % mod )
+
+if (x + y) % 3 == 0 :
+    moves = (x + y) // 3
+    tate = (moves + abs(x - y)) // 2
+
+    print(cmb(moves, tate, mod))
+else :
+    print(0)
