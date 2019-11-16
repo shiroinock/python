6f92f62ebdcd0c38ddbf3c9e303d2ba80f3854fa
shiroino <k.i.wrd.setuna.g@gmail.com>
2019-11-16 22:02:29 +0900

    update

--- AtCoder/01_ABC/100/145/c.py
+++ AtCoder/01_ABC/100/145/c.py
@@ -1,8 +1,16 @@
-'''n = int(input())
+n = int(input())
 
 towns = []
+count = 0
+lenSum = 0
 
 for i in range(n) :
     tmp = list(map(int, input().split()))
     towns.append(tmp)
-'''
+
+for p in range(len(towns)-1) :
+    for q in range(p+1, len(towns)) :
+        lenSum += ((towns[p][0] - towns[q][0]) ** 2 + (towns[p][1] - towns[q][1]) ** 2) ** 0.5
+        count += 1
+
+print(lenSum / count * (n - 1))
