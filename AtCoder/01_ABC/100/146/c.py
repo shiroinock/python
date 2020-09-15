acb6b26cb9b41ad7990ccff1b24fccbc5d513565
shiroinock <k.i.wrd.setuna.g@gmail.com>
2020-09-16 06:04:06 +0900

    update

--- AtCoder/01_ABC/100/146/c.py
+++ AtCoder/01_ABC/100/146/c.py
@@ -1,3 +1,25 @@
-a, b, x = map(int, input().split())
+A, B, X = map(int, input().split())
 
-にぶたんだこれ
+Nmax = 10 ** 9
+Nmin = 1
+Nmid = (Nmax + Nmin) // 2
+
+def nIsAffordable(a, b, n):
+    lenOfN = len(str(n))
+    price = a * n + b * lenOfN
+    if price <= X :
+        return True
+    else :
+        return False
+
+if nIsAffordable(A, B, Nmax) :
+    print(Nmax)
+else :
+    while Nmax != Nmin :
+        if nIsAffordable(A, B, Nmid) :
+            Nmin = Nmid + 1
+            Nmid = (Nmax + Nmin) // 2
+        else :
+            Nmax = Nmid
+            Nmid = (Nmax + Nmin) // 2
+    print(Nmid - 1)
---
+++ README.md
@@ -0,0 +1,3 @@
+メモ
+二分探索
+https://atcoder.jp/contests/abc146/tasks/abc146_c
